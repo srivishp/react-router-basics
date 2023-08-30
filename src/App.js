9da63e8ffca43686247c08414a23622bfa7a5dd2
Sri Vishnu <srivishp@gmail.com>
2023-08-31 00:12:29 +0530

    Define a route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+
+// Every object in this array represents a route
+const router = createBrowserRouter([
+  {
+    // https://example.com/path
+    // path can also be nothing (/)
+    path: "/",
+    // For dev server, it will be just localhost:3000 (http://localhost:3000/)
+
+    // JSX code to be rendered is the element
+    // element may also contain other jsx code
+    // like <p><HomePage/></p>
+    element: <HomePage />,
+  },
+]);
+
 function App() {
-  return <div></div>;
+  // Providing/Rendering the router on the screen, and load the active path
+  return <RouterProvider router={router} />;
 }
 
 export default App;
